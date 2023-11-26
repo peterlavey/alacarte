14d82639d6fa5f9dc7fd01a7ab986525212d5318
Pedro Cisternas <pedrocisternasd@gmail.com>
2023-11-26 19:10:16 -0300

    feat: file system

--- src/components/qr-reader/qrReader.js
+++ src/components/qr-reader/qrReader.js
@@ -1,27 +1,10 @@
 import React, {useEffect, useState} from "react";
-import {Html5QrcodeScanner, Html5QrcodeScanType} from "html5-qrcode";
-import {read, save} from "../../services/file";
-
-const config = {
-    fps: 10,
-    qrbox: {width: 100, height: 100},
-    rememberLastUsedCamera: true,
-    supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
-};
+import {read} from "../../services/file";
 
 const QrScanner = () => {
     const [ result, setResult ] = useState();
-    const init = () => {
-        let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
-        html5QrcodeScanner.render(decodedText => {
-            setResult(decodedText)
-        });
-        // html5QrCode.clear();
-    }
 
     useEffect(() => {
-        // init();
-        // save('test').then(r => console.log(r));
         read().then(r => setResult(r));
     }, []);
 
