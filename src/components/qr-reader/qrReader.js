0d0acec4a20076aa9068af096124938ab97d7750
Pedro Cisternas <pedrocisternasd@gmail.com>
2023-11-26 18:14:25 -0300

    chore: pending work defined

--- src/components/qr-reader/qrReader.js
+++ src/components/qr-reader/qrReader.js
@@ -1,21 +1,23 @@
 import React, {useEffect, useState} from "react";
 import {Html5QrcodeScanner, Html5QrcodeScanType} from "html5-qrcode";
 
+const config = {
+    fps: 10,
+    qrbox: {width: 100, height: 100},
+    rememberLastUsedCamera: true,
+    // Only support camera scan type.
+    supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
+};
+
 const QrScanner = () => {
     const [ result, setResult ] = useState();
     const onScanSuccess = (decodedText, decodedResult) => setResult(decodedResult);
 
     useEffect(() => {
-        let config = {
-            fps: 10,
-            qrbox: {width: 100, height: 100},
-            rememberLastUsedCamera: true,
-            // Only support camera scan type.
-            supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA]
-        };
-
         let html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", config, /* verbose= */ false);
+            "reader",
+            config,
+            false);
         html5QrcodeScanner.render(onScanSuccess);
         // html5QrCode.clear();
 
